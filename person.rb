0646749c3f4e34bd368cd1b52d3112d58a8b09e6
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-16 14:14:26 +0300

    update person.rb

--- person.rb
+++ person.rb
@@ -1,13 +1,13 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     if @age >= 18 || @parent_permission

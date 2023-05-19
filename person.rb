788bc7dac77db4c999d1cb9bc4451953dad007cb
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-19 11:39:19 +0300

    Update person

--- person.rb
+++ person.rb
@@ -7,9 +7,9 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1000)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission

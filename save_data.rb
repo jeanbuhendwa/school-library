ea5736f716e9276bcdb2855e196cec5e50173001
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-24 23:59:45 +0300

    Add people in the save_date

--- save_data.rb
+++ save_data.rb
@@ -1,5 +1,6 @@
 require 'json'
 require_relative 'book'
+require_relative 'person'
 require_relative 'load_data'
 
 # Save books into books json file
@@ -11,5 +12,12 @@ class SaveData
         json_book = JSON.generate(new_book)
         File.write('books.json', json_book)
       end
-      
+
+      def self.save_people(people)
+        new_people = people.each_with_index.map do |person, index|
+          { age: person.age, name: person.name, id: index, parent_permission: person.parent_permission }
+        end
+        json_people = JSON.generate(new_people)
+        File.write('people.json', json_people)
+      end
 end
